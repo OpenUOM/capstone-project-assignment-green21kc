9afe6f05d6deddab43ea063afa707c4930bea0da
green21kc <green21kc@gmail.com>
2024-05-09 09:39:58 +0530

    commentRemake2

--- backend/server.js
+++ backend/server.js
@@ -48,8 +48,7 @@ app.post("/getTeacherInfo", async function (req, res) {
 app.post("/addTeacher", async function (req, res) {
   let reqBody = req.body;
   console.log(
-    "Request received to add teacher hello. Req body: " +
-      JSON.stringify(reqBody)
+    "Request received to add teacher . Req body: " + JSON.stringify(reqBody)
   );
   let data = await addTeacher(reqBody.name, reqBody.age);
 
